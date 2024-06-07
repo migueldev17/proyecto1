785291d720b273f8ed3e7ab0ca216e6ed3ba2fec
migueldev17 <migueld.castanos017@gmail.com>
2024-06-07 16:25:38 -0500

    Relaciones de los modelos

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+    /**
+     * Relacion de uno a muchos
+     */
+    public function post(){
+        return $this->hasMany(Post::class);
+    }
+
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    /**
+     * relacion de uno a muvhos inversa
+     */
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+    /**
+     * Relacion de muchos a muchos
+     */
+    public function tags(){
+        return $this->belongToMany(Tag::class);
+    }
+    public function Image(){
+        return $this->morphOne(image::class,'imageable');
+        }
+    
+
+
 }
